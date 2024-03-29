7d9bb4fb7b6073c42da5f9fad463c0cfade4598d
Y_Fedotov <92554647+yuriyfedotov@users.noreply.github.com>
2024-03-29 16:39:12 +0300

    upload

---
+++ data.js
@@ -0,0 +1,319 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--",
+      "name": "Второй этаж",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -3.1410113367571384,
+        "pitch": 0.0378136055670133,
+        "fov": 1.4438271227947086
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.069387685792039,
+          "pitch": 0.05374036549681982,
+          "rotation": 0,
+          "target": "1----"
+        },
+        {
+          "yaw": -1.286918486754498,
+          "pitch": 0.006856998189114449,
+          "rotation": 0,
+          "target": "6--"
+        },
+        {
+          "yaw": 1.9241256106659854,
+          "pitch": 0.027876424376509945,
+          "rotation": 0,
+          "target": "5--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1----",
+      "name": "Основной зал у входа",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.11301870172876072,
+        "pitch": 0.08350752608029843,
+        "fov": 1.4438271227947086
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0206220172283995,
+          "pitch": 0.06575055100618599,
+          "rotation": 0,
+          "target": "0--"
+        },
+        {
+          "yaw": -0.02030179918149244,
+          "pitch": 0.18824998198663856,
+          "rotation": 0,
+          "target": "2---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2---",
+      "name": "Основной зал середина",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0.08056033119691719,
+        "pitch": -0.0619133656331865,
+        "fov": 1.4438271227947086
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0711771077638357,
+          "pitch": 0.028998662657388152,
+          "rotation": 0,
+          "target": "0--"
+        },
+        {
+          "yaw": -1.7025883034932772,
+          "pitch": 0.006075773942797014,
+          "rotation": 0,
+          "target": "5--"
+        },
+        {
+          "yaw": -1.6478485585158857,
+          "pitch": 0.006821768026796349,
+          "rotation": 0,
+          "target": "3------"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3------",
+      "name": "Зал -Изогнутый- у входа",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.3530935819501799,
+        "pitch": -0.007282860644615141,
+        "fov": 1.4438271227947086
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3584420345432608,
+          "pitch": 0.07876136044879267,
+          "rotation": 0,
+          "target": "2---"
+        },
+        {
+          "yaw": 1.3552672416715659,
+          "pitch": -0.06924129627996223,
+          "rotation": 0,
+          "target": "5--"
+        },
+        {
+          "yaw": -0.3242504164819202,
+          "pitch": 0.07245826602836658,
+          "rotation": 0,
+          "target": "4-----"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-----",
+      "name": "Зал -Изогнутый- середина",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -3.115832053411216,
+        "pitch": 0.0036397175844982144,
+        "fov": 1.4438271227947086
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.21499168988761497,
+          "pitch": 0.07088924652359196,
+          "rotation": 0,
+          "target": "3------"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5--",
+      "name": "Зал слева",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0.03087890215065947,
+        "pitch": -0.06554574580153627,
+        "fov": 1.4438271227947086
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2067989921349067,
+          "pitch": 0.010050834514032658,
+          "rotation": 0,
+          "target": "2---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6--",
+      "name": "Зал справа",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 3.027234567082308,
+        "pitch": 0.12399370397085008,
+        "fov": 1.4438271227947086
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2032785309453073,
+          "pitch": 0.03518277330050523,
+          "rotation": 0,
+          "target": "0--"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
